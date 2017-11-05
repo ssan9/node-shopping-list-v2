3ba6b60b21965070fe22bf0f19499f5d436f94fd
ssan9 <perrsy@gmail.com>
2017-11-05 11:58:10 -0500

    removed backslashes in fields

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.post('/recipes', jsonParser, (req, res) => {
   for (let i = 0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
+      const message = `Missing ${field} in request body`
       console.error(message);
       return res.status(400).send(message);
     }
